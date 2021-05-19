7abf1617bbe9180c8957af133ca275a30a8080a9
Carlos Herrera <herrerarangelcarloscbtis85@gmail.com>
2021-05-18 23:51:18 -0500

    1er commit

--- Archivos finales/CODIGOS FINALES/MODULO A/ESP/espFirmwareA/espFirmwareA.ino
+++ Archivos finales/CODIGOS FINALES/MODULO A/ESP/espFirmwareA/espFirmwareA.ino
@@ -1,22 +1,124 @@
-#include<SoftwareSerial.h>
-#include<ArduinoJson.h>
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <ArduinoJson.h>
+#include <SoftwareSerial.h>
 
-SoftwareSerial nano(D2, D4);
+#define rxPin 4
+#define txPin 2
 
-void setup ()
+const char* wifi = "VALCAR";
+const char* pass = "geagonzalez";
+const char* idCliente = "Modulo-A";
+const char* broker = "192.168.1.69";
+const int puerto = 1883;
+
+String mensajeJson;
+char* mensaje;
+bool mensajeListo = false;
+
+SoftwareSerial nano(rxPin, txPin);
+
+WiFiClient nodemcu;
+PubSubClient moduloA(nodemcu);
+
+void wifiConfig()
+{
+    Serial.println("");
+    Serial.print("Conectando a: ");
+    Serial.println(wifi);
+
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(wifi, pass);
+
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(500);
+        Serial.print("-");
+    }
+
+    randomSeed(micros());
+
+    Serial.println("Wifi conectado con exito");
+    Serial.print("Direccion IP: ");
+    Serial.println(WiFi.localIP());
+}
+
+void reconectar()
+{
+    while (!moduloA.connected())
+    {
+        Serial.print("Intentano conexion MQTT...");
+        
+        if (moduloA.connect(idCliente))
+        {
+            Serial.println("Conectado con exito");
+        }
+        else
+        {
+            Serial.print("Fallo en la conexion. ERROR: ");
+            Serial.println(moduloA.state());
+            Serial.println("Reintentando en 5s");
+            
+            delay(5000);
+        }
+    }
+}
+
+void setup()
 {
     Serial.begin(9600);
     nano.begin(9600);
+
+    wifiConfig();
+
+    moduloA.setServer(broker, puerto);
 }
 
-void loop ()
+void loop()
 {
-    DynamicJsonDocument doc(1024);
-    doc["tipo"] = "peticion";
-    serializeJson(doc, nano);
+    if (!moduloA.connected())
+    {
+        reconectar();
+    }
 
-    String mensaje = nano.readString();
+    while (nano.available())
+    {
+        mensajeJson = nano.readString();
+        mensajeListo = true;
+    }
+    
 
-    Serial.println(mensaje);
-    delay(1000);
+    if (mensajeListo)
+    {
+        DynamicJsonDocument datos(1024);
+        DeserializationError error = deserializeJson(datos, mensajeJson);
+    
+        if (error)
+        {
+            Serial.print("Error en la deserealizacion ERROR: ");
+            Serial.println(error.c_str());
+            mensajeListo = false;
+            return;
+        }
+        
+        String valA0 = datos["A0"];
+        String valA1 = datos["A1"];
+        String valA2 = datos["A2"];
+        String valA3 = datos["A3"];
+        
+        String valD0 = datos["D0"];
+        String valD1 = datos["D1"];
+        String valD2 = datos["D2"];
+        String valD3 = datos["D3"];
+
+        moduloA.publish("moduloA/A0", valA0.c_str());
+        moduloA.publish("moduloA/A1", valA1.c_str());
+        moduloA.publish("moduloA/A2", valA2.c_str());
+        moduloA.publish("moduloA/A3", valA3.c_str());
+
+        moduloA.publish("moduloA/D0", valD0.c_str());
+        moduloA.publish("moduloA/D1", valD1.c_str());
+        moduloA.publish("moduloA/D2", valD2.c_str());
+        moduloA.publish("moduloA/D3", valD3.c_str());
+    }
 }
--- Archivos finales/CODIGOS FINALES/MODULO A/NANO/nanoFirmwareA/nanoFirmwareA.ino
+++ Archivos finales/CODIGOS FINALES/MODULO A/NANO/nanoFirmwareA/nanoFirmwareA.ino
@@ -1,71 +1,55 @@
-#include <SoftwareSerial.h> //Libreria para comunicacion serial
-#include <ArduinoJson.h> //Libreria para Json
+#include <ArduinoJson.h>
+#include <SoftwareSerial.h>
 
-#define rxPin 2 //Pin de recepcion
-#define txPin 4 //Pin de transmision
+#define rxPin 2
+#define txPin 4
 
-//Pines analogicos
-
-#define A0 14 
+#define A0 14
 #define A1 15
 #define A2 16
-#define A3 16
-
-//Pines digitales
+#define A3 17
 
 #define D0 3
 #define D1 5
 #define D2 6
 #define D3 9
 
-//Variables
+SoftwareSerial nodemcu(rxPin, txPin);
 
-String mensaje = "";
-bool mensajeListo = false;
-
-SoftwareSerial nodemcu (rxPin, txPin); //Creacion de la comunicacion serial "nodemcu"
-
-void setup ()
+void setup()
 {
     Serial.begin(9600);
     nodemcu.begin(9600);
 }
 
-void loop ()
+void loop()
 {
-    while (nodemcu.available())
-    {
-        mensaje = nodemcu.readString();
-        mensajeListo = true;
-    }
-    if (mensajeListo)
-    {
-        DynamicJsonDocument documento(1024);
-        DeserializationError error = deserializeJson(documento, mensaje);
-        if (error)
-        {
-            Serial.print("Error en la deserializacion.    ERROR: ");
-            Serial.println(error.c_str());
-            mensajeListo = false;
-            return;
-        }
-        if (documento["tipo"] == "peticion")
-        {
-            documento["tipo"] = "respuesta";
+    delay(10);
 
-            documento["A0"] = analogRead(A0);
-            documento["A1"] = analogRead(A1);
-            documento["A2"] = analogRead(A2);
-            documento["A3"] = analogRead(A3);
+    DynamicJsonDocument datos(1024);
 
-            documento["D0"] = digitalRead(D0);
-            documento["D1"] = digitalRead(D1);
-            documento["D2"] = digitalRead(D2);
-            documento["D3"] = digitalRead(D3);
+    int valA0 = analogRead(A0);
+    int valA1 = analogRead(A1);
+    int valA2 = analogRead(A2);
+    int valA3 = analogRead(A3);
 
-            serializeJson(documento, nodemcu);
-        }
-        mensajeListo = false;
-    }
-    
+    int valD0 = digitalRead(D0);
+    int valD1 = digitalRead(D1);
+    int valD2 = digitalRead(D2);
+    int valD3 = digitalRead(D3);
+
+    datos["A0"] = valA0;
+    datos["A1"] = valA1;
+    datos["A2"] = valA2;
+    datos["A3"] = valA3;
+
+    datos["D0"] = valD0;
+    datos["D1"] = valD1;
+    datos["D2"] = valD2;
+    datos["D3"] = valD3;
+
+    serializeJson(datos, nodemcu);
+    serializeJson(datos, Serial);
+    Serial.println("");
+    delay(1000);
 }
